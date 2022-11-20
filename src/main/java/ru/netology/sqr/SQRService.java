7cab9ed32c9da567f2a913b9c27d452a30a96adb
Oleg <zkv_testpost@mail.ru>
2022-11-20 15:42:05 +0300

    sqr

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,10 +3,11 @@ package ru.netology.sqr;
 public class SQRService {
     public int calcSqrt(int min, int max) {
         int result = 0;
-        for (int i = 10; i < 99; i++)
+        for (int i = 10; i < 99; i++) {
             if (i * i >= min && i * i <= max) {
                 result++;
             }
+        }
         return result;
     }
 }
